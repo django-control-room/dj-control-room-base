45587f592182612b54f54541ed9b45821a76bd5a
Yasser Toruno <yasser@yassers-mbp.lan>
2026-04-08 00:31:46 -0400

    create a context creation utility to hide admin detail from panel authors

--- dj_control_room_base/views.py
+++ dj_control_room_base/views.py
@@ -1,20 +1,13 @@
 from django.contrib.admin.views.decorators import staff_member_required
 from django.shortcuts import render
-from django.contrib import admin
 
-from dj_control_room_base.conf import get_css_context
+from dj_control_room_base.conf import DEFAULTS, SETTINGS_KEY
+from dj_control_room_base.core.context import get_panel_context
 
 
 @staff_member_required
 def index(request):
-    """
-    Display panel dashboard.
-    """
-    context = admin.site.each_context(request)
-    context.update(get_css_context())
-    context.update(
-        {
-            "title": "Dj Control Room Base",
-        }
+    context = get_panel_context(
+        request, SETTINGS_KEY, defaults=DEFAULTS, title="Dj Control Room Base"
     )
     return render(request, "admin/dj_control_room_base/index.html", context)
