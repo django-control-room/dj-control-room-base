4e969805ce0975fa6fd9b198fbf133cd873edee6
Yasser Toruno <yasser@yassers-mbp.lan>
2026-06-01 14:24:56 -0400

    define a new panel plugin class to stop relying so much on duck typing

--- dj_control_room_base/core/__init__.py
+++ dj_control_room_base/core/__init__.py
@@ -1,10 +1,12 @@
 from dj_control_room_base.core.admin import BasePanelAdmin
 from dj_control_room_base.core.models import PanelPlaceholderModel
 from dj_control_room_base.core.panel_config import PANEL_BUILTIN_DEFAULTS, PanelConfig
+from dj_control_room_base.core.panel_plugin import PanelPlugin
 
 __all__ = [
     "BasePanelAdmin",
     "PANEL_BUILTIN_DEFAULTS",
     "PanelConfig",
     "PanelPlaceholderModel",
+    "PanelPlugin",
 ]
