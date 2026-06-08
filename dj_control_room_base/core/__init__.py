62292ddb6356bd3da8f8b374e69659bbec90f9b8
Yasser Toruno <yasser@yassers-mbp.lan>
2026-06-07 20:33:04 -0400

    fix base panel admin import after name change

--- dj_control_room_base/core/__init__.py
+++ dj_control_room_base/core/__init__.py
@@ -1,4 +1,4 @@
-from dj_control_room_base.core.admin import BasePanelAdmin
+from dj_control_room_base.core.panel_admin import BasePanelAdmin
 from dj_control_room_base.core.models import PanelPlaceholderModel
 from dj_control_room_base.core.panel_config import PANEL_BUILTIN_DEFAULTS, PanelConfig
 from dj_control_room_base.core.panel_plugin import PanelPlugin
