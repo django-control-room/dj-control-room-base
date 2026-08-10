21357b7ac1e995cf69efe61d571b05679935e59c
Yasser Toruno <yasser@yassers-mbp.lan>
2026-08-10 13:05:06 -0400

    add general light and dark adapters

--- dj_control_room_base/core/__init__.py
+++ dj_control_room_base/core/__init__.py
@@ -3,6 +3,10 @@ from dj_control_room_base.core.models import PanelPlaceholderModel
 from dj_control_room_base.core.panel_config import (
     PANEL_BUILTIN_DEFAULTS,
     THEME_ADAPTER_PATHS,
+    THEME_GENERAL_DARK_APPS,
+    THEME_GENERAL_DARK_PATH,
+    THEME_GENERAL_LIGHT_APPS,
+    THEME_GENERAL_LIGHT_PATH,
     PanelConfig,
     detect_theme_adapter_path,
 )
@@ -12,6 +16,10 @@ __all__ = [
     "BasePanelAdmin",
     "PANEL_BUILTIN_DEFAULTS",
     "THEME_ADAPTER_PATHS",
+    "THEME_GENERAL_DARK_APPS",
+    "THEME_GENERAL_DARK_PATH",
+    "THEME_GENERAL_LIGHT_APPS",
+    "THEME_GENERAL_LIGHT_PATH",
     "PanelConfig",
     "PanelPlaceholderModel",
     "PanelPlugin",
